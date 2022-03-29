50d14a8ba66a65dfccce4cb0622e28d9401f4cfe
Tungsinn <gvnalamas@tutanota.com>
2022-03-29 19:05:10 -0300

    Connect to MongoDB and start server

--- src/app/index.js
+++ src/app/index.js
@@ -1,16 +1,17 @@
 // import path from 'path';
 // import cors from 'cors';
 const express = require('express');
-require('dotenv').config();
 // import routes from './routes/routes';
-// import connectToMongoDB from './database';
+const connectToMongoDB = require('./database');
 
+require('dotenv').config();
 const app = express();
+connectToMongoDB();
 
-//connectToMongoDB();
-//app.use(cors());
+// app.use(cors());
+// app.use(express.json());
+
 //app.use('/images', express.static(path.resolve(__dirname, '..', '..', 'uploads')));
-app.use(express.json());
 //app.use(routes);
 
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,4 +1,3 @@
-const express = require('express');
 // const path = require('path');
 // const routes = require('./routes');
 const app = require('./app');
@@ -6,9 +5,4 @@ const app = require('./app');
 require('dotenv').config();
 
 const PORT = process.env.PORT || process.env.LOCAL_PORT;
-
-// app.on('ready', () => {
-//   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-// });
-
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
